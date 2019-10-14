a26894027a078c20530c10a452357ce6410d2ed8
Administrator <Administrator@PC201806101645>
2019-10-14 11:23:41 +0800

    feign 服务第一次全量提交

---
+++ servernode/src/main/java/com/pactera/data/governance/ServerNodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pactera.data.governance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerNodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerNodeDemoApplication.class, args);
+	}
+
+}
