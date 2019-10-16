da77ea52c38b5ee2725f42057a20f5e48e713607
Administrator <Administrator@192.168.60.1>
2019-10-16 11:08:56 +0800

    微服务节点增加日志输出到rabbitmq功能

--- dataqualityservernode/src/main/java/com/pactera/data/governance/controller/TestController.java
+++ dataqualityservernode/src/main/java/com/pactera/data/governance/controller/TestController.java
@@ -1,13 +1,8 @@
 package com.pactera.data.governance.controller;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,13 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/test")
 public class TestController {
 	
-	@Autowired
-	HttpServletRequest request;
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class); 
 	
 	@ResponseBody
 	@RequestMapping(value = "/testsyh102")
 	public String test(@RequestParam(value = "uname") String uname) {
-		
+		logger.info("***********Start Logging to rabbitMQ************");        
+	    logger.info("Publish Welcome Message to RabbitMQ"); 
 		return "iiik2>>>>>>"+uname;
 	}
 	
